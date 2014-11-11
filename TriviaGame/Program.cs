3ca276e06cfd794f81f2106a51287065ef7c8f6e
Dustin Kraft <kraftdr@gmail.com>
2014-11-11 15:21:27 -0700

    moved trivia class to program.cs

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -33,4 +33,20 @@ namespace TriviaGame
             return returnList;
         }
     }
+
+    class Trivia
+    {
+        //TODO: Fill out the Trivia Object
+
+        //The Trivia Object will have 2 properties
+        // at a minimum, Question and Answer
+
+        //The Constructor for the Trivia object will
+        // accept only 1 string parameter.  You will
+        // split the question from the answer in your
+        // constructor and assign them to the Question
+        // and Answer properties
+
+
+    }
 }
