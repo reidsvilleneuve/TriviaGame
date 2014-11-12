60294368667f81cd9cd90c6450f948b49976d3d8
reidsvilleneuve <reidsvilleneuve@gmail.com>
2014-11-12 16:20:20 -0700

    Created classes, populated list.
    TODO: Add category to Trivia class. No ":" means bonus questions or
    throwaway value. TODO: Make sure that Name That Dog is disincluded from
    being presented to user.

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -13,6 +13,19 @@ namespace TriviaGame
         {
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
+
+            int score = 0;
+            int highScore = 0;
+            bool playAgain = true;
+
+            while(playAgain)
+            {
+                score = triviaRound(AllQuestions);
+
+                if (score > highScore) highScore = score;
+                Console.WriteLine("Your score: {0}\nHigh score: {1}", score, highScore);
+            }
+            
         }
 
 
@@ -26,11 +39,27 @@ namespace TriviaGame
             
             //make a new list to return all trivia questions
             List<Trivia> returnList = new List<Trivia>();
+
+
+            foreach (string i in contents)
+                returnList.Add(new Trivia(i));
+
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
             //Return the full list of trivia questions
             return returnList;
+        }
+
+        /// <summary>
+        /// Initiates a game of Trivia. Looping is handled here and score is returned.
+        /// </summary>
+        /// <param name="categorizedQuestions">An Object array in the format of: string catagory, Trivia(question/answer)</param>
+        /// <returns>Score - answers correct / time.</returns>
+        static int triviaRound(List<Trivia> questionList)
+        {
+
+            return 0;
         }
     }
 
@@ -46,6 +75,57 @@ namespace TriviaGame
         // split the question from the answer in your
         // constructor and assign them to the Question
         // and Answer properties
+
+
+        //Properties
+
+        private string _question;
+        public string Question
+        {
+            get { return _question; }
+            set { _question = value; }
+        }
+
+        private string _answer;
+        public string Answer
+        {
+            get { return _answer; }
+            set { _answer = value; }
+        }
+
+        //Constructors
+
+        public Trivia()
+        {
+            this.Question = "Undefined";
+            this.Answer = "Undefined";
+        }
+
+        public Trivia(string question, string answer)
+        {
+            this.Question = question;
+            this.Answer = answer;
+        }
+
+        public Trivia(string questionAsteriskAnswer)
+        {
+            List<string> splitList = questionAsteriskAnswer.Split('*').ToList();
+
+            this.Question = splitList[0];
+            this.Answer = splitList[1];
+        }
+
+        public Trivia(List<string> questionAnswer)
+        {
+            this.Question = questionAnswer[0];
+            this.Answer = questionAnswer[1];
+        }
+
+        public Trivia(Trivia triviaEntry)
+        {
+            this.Question = triviaEntry.Question;
+            this.Answer = triviaEntry.Answer;
+        }
 
 
     }
